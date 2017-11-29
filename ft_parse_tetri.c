f5421491723979657fb903491f0ac58ac33c3af5
Joffrey JANIEC <jjaniec@e3r6p4.42.fr>
2017-11-29 19:36:11 +0100

    bugfix parse tetri

--- ft_parse_tetri.c
+++ ft_parse_tetri.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/29 15:47:50 by jjaniec           #+#    #+#             */
-/*   Updated: 2017/11/29 17:02:51 by jjaniec          ###   ########.fr       */
+/*   Updated: 2017/11/29 19:35:33 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,36 +17,23 @@ char	*ft_strsub(char *s, int a, int b);
 char	*ft_parse_tetri(char *s, int nb)
 {
 	int		i;
-	int		j;
 
 	i = 0;
-	j = 0;
 	while (s[i] != '#' && s[i] != '.')
 		i++;
-	while (s[i++])
+	while (s[i])
 	{
 		if ((s[i] == '#' || s[i] == '.') && nb != 1)
 		{
-			while (s[i] == '#' || s[i] == '.' || s[i] == '\n' && \
-					s[i - 1] != '\n')
+			while (!(s[i] == '\n' && s[i + 1] == '\n') && s[i + 1])
+				i++;
+			while (s[i] == '\n')
 				i++;
 			nb--;
-			if (s[i] == '\n' && s[i - 1] == '\n')
-				i++;
 		}
 		if (nb == 1)
 			return (ft_strsub(s, i, 20));
+		i++;
 	}
 	return (NULL);
-}
-
-int		main()
-{
-	char	*buf;
-	int		fd;
-
-	fd = open("map.fillit", O_RDONLY);
-	buf = ft_read_content(fd);
-	buf = ft_parse_tetri(buf, 4);
-	printf("\nreturned buf\n//////////\n|%s|", buf);
 }
