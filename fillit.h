af048a7a884d07674a15de69d47e0f6d173cc705
Ugo Nicolai <unicolai@student.42.fr>
2017-11-29 18:22:20 +0100

    add struc

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 15:14:22 by jjaniec           #+#    #+#             */
-/*   Updated: 2017/11/28 15:51:37 by jjaniec          ###   ########.fr       */
+/*   Updated: 2017/11/29 16:37:52 by unicolai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,14 @@
 # include <fcntl.h>
 # include <strings.h>
 
-# define BUFF_SIZE 25
+# define BUFF_SIZE 21
 
-char	*ft_read_content(int fd);
-
-char	*ft_strjoin(char const *s1, char const *s2);
+typedef struct	s_tetri
+{
+	int		x;
+	int		y;
+	char	*s;
+}				t_tetri;
+char			*ft_read_content(int fd);
+char			*ft_strjoin(char const *s1, char const *s2);
 #endif
