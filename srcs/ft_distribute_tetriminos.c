be75d3803c5239b8896bdcfb55a50dd39c86aad6
vm <jjaniec@student.42.fr>
2017-12-24 09:46:37 -0800

    init t_tetri.decaltetri to 0

--- srcs/ft_distribute_tetriminos.c
+++ srcs/ft_distribute_tetriminos.c
@@ -18,13 +18,14 @@ int		ft_distribute_tetriminos(t_tetri **tab, char *s)
 	int		j;
 
 	j = ft_count_tetris(s);
-	(*tab) = (t_tetri *)malloc(sizeof(t_tetri) * j + 1);
+	(*tab) = (t_tetri *)malloc(sizeof(t_tetri) * (j + 1));
 	i = -1;
 	while (++i != j)
 	{
 		(*tab)[i].s = ft_parse_tetri(s, i + 1);
 		ft_upleft((*tab)[i].s);
-		printf("Struct %d -> |\n%s|", i, (*tab)[i].s);
+		(*tab)[i].decaltetri = 0;
+		printf("Struct %d/%d -> |\n%s|", i, j, (*tab)[i].s);
 	}
 	(*tab)[j].s = NULL;
 	return (0);
